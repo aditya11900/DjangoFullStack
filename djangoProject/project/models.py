2238086517d08ec9387a4cb540cb2e06248c352b
aditya11900 <adityaraj153566@gmail.com>
2024-07-02 02:56:00 +0530

    Models

--- djangoProject/project/models.py
+++ djangoProject/project/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class ProjectVariety(models.Model):
+    PROJECT_TYPE = [
+        ('web', 'Web Development'),
+        ('mobile', 'Mobile Development'),
+        ('desktop', 'Desktop Development'),
+        ('game', 'Game Development'),
+        ('other', 'Other Development'),
+    ]
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to="img/")
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=PROJECT_TYPE)
+    
